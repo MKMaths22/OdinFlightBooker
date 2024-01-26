baca5269a4977a8a00099019eebf42f5a57ac2c9
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-01-26 10:53:39 +0000

    Remove dependent: :destroy commands from Airport model to allow new seeds file to create four airports using db:setup

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,6 +1,6 @@
 class Airport < ApplicationRecord
-  has_many :departing_flights, foreign_key: 'departure_airport_id', dependent: :destroy
-  has_many :arriving_flights, foreign_key: 'arrival_airport_id', dependent: destroy
+  has_many :departing_flights, foreign_key: 'departure_airport_id'
+  has_many :arriving_flights, foreign_key: 'arrival_airport_id'
   validates :code, presence: true
   validates :code, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+    
+    Airport.destroy_all
+
+    ["KYN", "LBA", "LHR", "LTN"].each do |code|
+      Airport.create!(code: code)
+    end
