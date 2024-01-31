6270d449f268fcd1642cdfaef34ad0de4dfd6d54
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-01-31 16:14:26 +0000

    First try at creating multiple Passenger objects in the Booking #create method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,13 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
   end
 
   def create
+    passenger_hash = Hash.new()
+    params[:number_of_passengers].times do |i|
+      passenger_hash[i] = Passenger.new
+    end
   end
 
   def show
