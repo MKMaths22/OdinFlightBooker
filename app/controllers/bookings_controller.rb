794c705a7a0648880ac45a4918ce33c9923a789e
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-02-01 11:53:18 +0000

    Flesh out #new and #create methods in Bookings controller. Add link_to "Start Again" and "Details for Passenger" headings in Bookings new view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,20 @@
 class BookingsController < ApplicationController
   def new
+    @flight = Flight.find(params[:flight_id])
     @booking = Booking.new
+    @passenger_hash = Hash.new()
+    params[:number_of_passengers].to_i.times do |i|
+    @passenger_hash[i] = Passenger.new
+    end
   end
 
   def create
-    passenger_hash = Hash.new()
-    params[:number_of_passengers].times do |i|
-      passenger_hash[i] = Passenger.new
+    @booking = Booking.new
+    @booking.update_attribute(:flight_id, params[:flight_id])
+    @booking.save
+    @passenger_hash = Hash.new()
+    params[:number_of_passengers].to_i.times do |i|
+    @passenger_hash[i] = Passenger.create([passengers_attributes[i]])
     end
   end
 
