a4b8b6bd86b96541c8305e53ac34a645270e3920
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-02-02 10:22:43 +0000

    Tinker with Bookings controller #create method to check what is going on. Use index: i in new Bookings view to get good params submitted from the form.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,12 +10,16 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new
+    number_of_passengers = params[:booking][:number_of_passengers]
+    puts "number of passengers = #{number_of_passengers}"
+    3.times do |i|
+    puts "The value of i is #{i}"
+    @passenger_hash[i] = Passenger.create([passengers_attributes[i]])
+    puts "passenger_hash is #{@passenger_hash}"
+    end
     @booking.update_attribute(:flight_id, params[:flight_id])
     @booking.save
-    @passenger_hash = Hash.new()
-    params[:number_of_passengers].to_i.times do |i|
-    @passenger_hash[i] = Passenger.create([passengers_attributes[i]])
-    end
+    redirect_to root_path
   end
 
   def show
