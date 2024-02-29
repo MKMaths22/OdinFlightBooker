15047b780c3553fedf678e069d2e24d6e0a58605
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-02-29 15:28:18 +0000

    Make messy progress towards getting attributes from clone to change X to the right number. Add classes to labels and inputs so they are easier to find in Javascript.

--- app/javascript/controllers/add_passenger_controller.js
+++ app/javascript/controllers/add_passenger_controller.js
@@ -9,15 +9,23 @@ export default class extends Controller {
     this.number_of_forms = document.querySelectorAll("h3").length
   }
 
+  correct_string(string) {
+    string.replace("X", `${this.number_of_forms}`)
+    // Haven't managed to call this function within add_form because of scope issues? 
+    // Will put off trying to fix this for now.
+  }
+
   add_form() {
     const passenger_details = document.querySelector("div.passengersdetailsdiv");
     const template = document.querySelector("#onepassenger");
     const clone = template.content.cloneNode(true);
-    
+    const first_label = clone.querySelector("label.namelabel");
+    const first_for_string = first_label.getAttribute("for");
+    console.log(first_for_string)
+    // first_label.setAttribute
     this.number_of_forms++;
     const headline = clone.querySelector("h3");
-    headline.textContent = `Details for Passenger ${this.number_of_forms}`
+    headline.textContent = headline.textContent.replace("X", `${this.number_of_forms}`)
     passenger_details.appendChild(clone)
   }
-
 }
