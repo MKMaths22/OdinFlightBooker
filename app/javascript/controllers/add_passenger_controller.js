6154e4ee18493f1b376d22c93032a92aeeccf21b
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-03-02 11:43:06 +0000

    Use address variable and pushState to change url..
    when #add_form so that if user fills in form wrongly the page
    will keep the same number of forms. Remove this.number_of_forms variable
    which cannot be consistent across different Stimulus controllers. Instead get #add_form
    and new #remove_form methods to use their own internal let variable for counting forms.
    Create remove_button button in the new Bookings view, getting it to remove its "div.onepassengerdiv"
    grandparent element, without correcting attributes of remaining form fields.

--- app/javascript/controllers/add_passenger_controller.js
+++ app/javascript/controllers/add_passenger_controller.js
@@ -2,12 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   
-  static values = { number_of_forms: Number }
-  static targets = [ "add_button" ]
-
-  connect() {
-    this.number_of_forms = document.querySelectorAll("h3").length
-  }
+  static targets = [ "add_button", "remove_button" ]
 
   // correct_string(string) {
   //   string.replace("X", `${this.number_of_forms}`)
@@ -16,28 +11,58 @@ export default class extends Controller {
   // }
 
   add_form() {
-    const correct_string = function(string, number) {
-      string.replace("X", `${number.to_s}`)
-    };
+    // const correct_string = function(string, number) {
+    //  string.replace("X", `${number.to_s}`)
+    //};
+    let number_of_forms = document.querySelectorAll("div.onepassengerdiv").length
     const passenger_details = document.querySelector("div.passengersdetailsdiv");
     const template = document.querySelector("#onepassenger");
     const clone = template.content.cloneNode(true);
 
-    const first_label = clone.querySelector("label.namelabel");
+    let first_label = clone.querySelector("label.namelabel");
     let old_stuff = first_label.getAttribute("for");
-    // let new_attribute = old_stuff.replace("X",`${this.number_of_forms}`)
-    let new_attribute = correct_string(old_stuff, this.number_of_forms);
+    let new_attribute = old_stuff.replace("X",`${number_of_forms}`)
+    // let new_attribute = correct_string(old_stuff, this.number_of_forms);
     first_label.setAttribute("for", `${new_attribute}`)
-    const first_name = clone.querySelector("input.nameinput");
+    let first_name = clone.querySelector("input.nameinput");
     first_name.setAttribute("id", `${new_attribute}`)
     old_stuff = first_name.getAttribute("name");
-    new_attribute = old_stuff.replace("X", `${this.number_of_forms}`)
+    new_attribute = old_stuff.replace("X", `${number_of_forms}`)
     first_name.setAttribute("name", `${new_attribute}`)
 
-
-    this.number_of_forms++;
+    first_label = clone.querySelector("label.emaillabel");
+    old_stuff = first_label.getAttribute("for");
+    new_attribute = old_stuff.replace("X",`${number_of_forms}`)
+    // let new_attribute = correct_string(old_stuff, this.number_of_forms);
+    first_label.setAttribute("for", `${new_attribute}`)
+    first_name = clone.querySelector("input.emailinput");
+    first_name.setAttribute("id", `${new_attribute}`)
+    old_stuff = first_name.getAttribute("name");
+    new_attribute = old_stuff.replace("X", `${number_of_forms}`)
+    first_name.setAttribute("name", `${new_attribute}`)
+    number_of_forms++;
+    console.log(number_of_forms);
     const headline = clone.querySelector("h3");
-    headline.textContent = headline.textContent.replace("X", `${this.number_of_forms}`)
-    passenger_details.appendChild(clone)
+    headline.textContent = headline.textContent.replace("X", `${number_of_forms}`);
+    passenger_details.appendChild(clone);
+    const para = document.querySelector("p.toppara");
+    para.textContent = para.textContent.replace(`${number_of_forms - 1}`, `${number_of_forms}`)
+    // the value of params would only change with a redirect, so we have to modify the 'You are making a booking for...' message here
+    let address = window.location.href;
+    address = address.replace(`passengers=${number_of_forms - 1}`, `passengers=${number_of_forms}`)
+    window.history.pushState(null, "", address);
+  }
+
+  remove_form() {
+    let number_of_forms = document.querySelectorAll("div.onepassengerdiv").length
+    if (number_of_forms == 1) {
+      return
+    }
+    else {
+      const the_button_clicked = this.remove_buttonTarget;
+      the_button_clicked.parentElement.parentElement.remove();
+      number_of_forms--;
+      console.log(number_of_forms);
+    }
   }
 }
